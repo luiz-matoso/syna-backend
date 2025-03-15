021e0a7162f03c3c391f3186778c56949f4fadf3
unknown <luizhmatoso1340@gmail.com>
2025-03-15 14:46:22 -0300

    First commit

---
+++ src/main/java/com/syna/url_shortner/UrlShortnerSynaApplication.java
@@ -0,0 +1,13 @@
+package com.syna.url_shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerSynaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerSynaApplication.class, args);
+	}
+
+}
