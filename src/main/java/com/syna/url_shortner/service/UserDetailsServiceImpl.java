d2ca4c26bae01d964e4284b1f90cd06593e33f49
unknown <luizhmatoso1340@gmail.com>
2025-03-15 16:27:49 -0300

    Add UserDetailsService & UserRepository

--- src/main/java/com/syna/url_shortner/service/UserDetailsServiceImpl.java
+++ src/main/java/com/syna/url_shortner/service/UserDetailsServiceImpl.java
@@ -1,5 +1,9 @@
 package com.syna.url_shortner.service;
 
+import com.syna.url_shortner.models.User;
+import com.syna.url_shortner.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,10 +11,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " not found"));
 
-
-        return null;
+        return UserDetailsImpl.build(user);
     }
 }
